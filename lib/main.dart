d2284b73722a7ea568c2c3c5401acb363d6be883
Fina Blomberg <finablomberg@hotmail.com>
2021-11-17 13:10:34 +0100

    Fixat så man kan klicka i individuella checkboxes. Ta bort fungerar dock inte.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 // ignore_for_file: unused_element
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'addView.dart';
 
+class MyState extends ChangeNotifier {
+  final List<CheckBoxState> _list = [];
+
+  List<CheckBoxState> get list => _list;
+
+  void removeBox(CheckBoxState checkbox) {
+    _list.remove(checkbox);
+    notifyListeners();
+  }
+}
+
+class CheckBoxState {
+  final String title;
+  bool value;
+
+  CheckBoxState({
+    required this.title,
+    this.value = false,
+  });
+}
+
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MyState(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -32,16 +59,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isChecked = false;
-  bool pressed = false;
-
-  /*  Map<String, bool?> tasks = {
-    'Write a book': false,
-    'Clean': false,
-    'Do dishes': false,
-    'Sweep floor': false,
-    'Climb Mount Everest': true,
-  }; */
+  final list = [
+    CheckBoxState(title: 'Read a book'),
+    CheckBoxState(title: 'Do homework'),
+    CheckBoxState(title: 'Tidy room'),
+    CheckBoxState(title: 'Have fun'),
+    CheckBoxState(title: 'Meditate'),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -62,7 +86,11 @@ class _MyHomePageState extends State<MyHomePage> {
               icon: const Icon(Icons.more_vert_sharp, size: 35))
         ],
       ),
-      body: _body(),
+      body: Consumer<MyState>(builder: (context, state, child) {
+        return ListView(children: <Widget>[
+          ...list.map(_buildCheckbox).toList(),
+        ]);
+      }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(context, MaterialPageRoute(builder: (context) {
@@ -80,120 +108,24 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  _body() {
-    return Container(
-      margin: const EdgeInsets.only(top: 10),
-      child: Column(
-        children: [
-          _item('Write a book'),
-          _divider(),
-          _item('Do Homework'),
-          _divider(),
-          _item('Tidy room'),
-          _divider(),
-          _item('Watch TV'),
-          _divider(),
-          _item('Nap'),
-          _divider(),
-          _item('Shop groceries'),
-          _divider(),
-          _item('Have fun'),
-          _divider(),
-          _item('Meditate'),
-          _divider(),
-        ],
-      ),
-    );
-  }
-
-/*   _checkItem2() {
-    final List<CheckboxItem> checkboxList = [
-      CheckboxItem('Test', false),
-      CheckboxItem('Test', false)
-    ];
-    return ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) {
-          return CheckboxListTile(
-              title: Text(checkboxList[index].text),
-              value: checkboxList[index].flag,
-              onChanged: (bool? value) {
-                setState(() {});
-              });
-        });
-  } */
-/* 
-  _checkItem() {
-    return Expanded(
-      child: Column(
-        children: tasks.keys.map(
-          (taskItem) {
-            return CheckboxListTile(
-              title: Text(
-                taskItem,
-                style: taskItem. == true 
-                ? const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w300,
-                  
-                )
-                :
-              ),
-              value: tasks[taskItem],
-              secondary: IconButton(
-                icon: const Icon(Icons.clear),
-                onPressed: () {
-                  setState(() {
-                    print(taskItem);
-/*  tasks.removeWhere((key, value) => value == tasks[taskItem]);*/
-                  });
-                },
-              ),
-              controlAffinity: ListTileControlAffinity.leading,
-              onChanged: (bool? value) {
-                setState(() {
-                  tasks[taskItem] = value;
-                });
-              },
-            );
-          },
-        ).toList(),
-      ),
-    );
-  } */
-
-  Widget _item(String name) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        Checkbox(
-          value: isChecked,
-          onChanged: (bool? value) {
-            setState(() {
-              isChecked = value!;
-              pressed = !pressed;
-            });
-          },
+  Widget _buildCheckbox(CheckBoxState checkbox) => CheckboxListTile(
+        contentPadding: const EdgeInsets.all(12.0),
+        controlAffinity: ListTileControlAffinity.leading,
+        value: checkbox.value,
+        title: Text(checkbox.title,
+            style: TextStyle(
+                decoration:
+                    checkbox.value ? TextDecoration.lineThrough : null)),
+        secondary: IconButton(
+            icon: const Icon(Icons.clear),
+            onPressed: () {
+              Provider.of<MyState>(context, listen: false).removeBox(checkbox);
+              /* state.removeBox(checkbox); */ //ta bort kort
+            }),
+        onChanged: (value) => setState(
+          () => checkbox.value = value!,
         ),
-        Expanded(
-          child: Text(
-            name,
-            style: pressed
-                ? const TextStyle(
-                    fontSize: 26,
-                    fontWeight: FontWeight.w400,
-                    decoration: TextDecoration.lineThrough,
-                  )
-                : const TextStyle(fontSize: 26, fontWeight: FontWeight.w400),
-          ),
-        ),
-        IconButton(
-          onPressed: () {},
-          icon: const Icon(Icons.clear),
-        ),
-      ],
-    );
-  }
+      );
 
   _divider() {
     return const Divider(
@@ -202,12 +134,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-/* class CheckboxItem {
-  final String text;
-  final bool flag;
-
-  CheckboxItem(this.text, this.flag);
-} */
-
-
